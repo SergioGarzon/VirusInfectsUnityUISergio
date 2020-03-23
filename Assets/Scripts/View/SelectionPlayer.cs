554148b8c2ccfd0534a2a56702cf67ccf4b352a1
Sergio <sggarzonutn@gmail.com>
2020-03-23 00:27:17 -0300

    Octava Subida

---
+++ Assets/Scripts/View/SelectionPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionPlayer : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+    
+
+    void Update()
+    {
+        
+    }
+}
