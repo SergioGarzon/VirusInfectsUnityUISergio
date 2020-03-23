096859142c303ec06c8daebc14cd6f7abab95d68
Sergio <sggarzonutn@gmail.com>
2020-03-23 02:33:52 -0300

    New Version

---
+++ Assets/Scripts/SelectPlayerControl/CharacterInformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInformation : MonoBehaviour
+{
+    public string characterType;
+
+    [HideInInspector] public string namePlayer;
+
+    public string descriptionPlayer;
+}
+
--- Assets/Scripts/SelectPlayerControl/SelectionPlayer.cs
+++ Assets/Scripts/SelectPlayerControl/SelectionPlayer.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class SelectionPlayer : MonoBehaviour
 {
 
+    public GameObject objectAlquimist;
+    public GameObject objectHacker;
+    private Vector3 vecSelection;
+
+
 
     void Start()
     {
