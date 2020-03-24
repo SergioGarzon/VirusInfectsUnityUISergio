96ec4a0ee986e9f96819126612befc641324505b
Sergio <sggarzonutn@gmail.com>
2020-03-24 01:52:43 -0300

    Nueva version

---
+++ Assets/Scripts/SelectPlayerControl/CharacterInfo.cs
@@ -0,0 +1,16 @@
+﻿using CharacterSelector.Script;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+
+    public string CharacterType;
+
+    [HideInInspector]
+    public string Name;
+
+    public string Description;
+
+}
