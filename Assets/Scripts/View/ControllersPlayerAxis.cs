59f64849d725e121673a9895de4b83e39a674d33
Sergio <sggarzonutn@gmail.com>
2020-03-27 04:42:55 -0300

    Se insertan nuevos paneles donde muestran los menu de opciones para el player

---
+++ Assets/Scripts/View/ControllersPlayerAxis.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllersPlayerAxis : MonoBehaviour
+{
+    public InputField inputHorizontal;
+
+}
+
