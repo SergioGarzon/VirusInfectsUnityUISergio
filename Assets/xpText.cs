cfc4c515cbc1a8fb56221b451b7e0838cacbd24a
Laura <lauravelazquez.lnv@gmail.com>
2020-04-21 19:30:56 -0300

    xp text

--- Assets/xpText.cs
+++ Assets/xpText.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class xpText : MonoBehaviour
 {
+    private Text _xpText;
+    [SerializeField] private ScoreData scoreData;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _xpText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _xpText.text="XP: "+ scoreData.xp;
     }
 }
