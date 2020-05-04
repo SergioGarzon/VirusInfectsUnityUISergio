2cfcdd77dfc26e8144765b0e997124a1da94d7a2
SERGIO <sggarzonutn@gmail.com>
2020-05-04 12:50:24 -0300

    Nuevos cambios

--- Assets/Scripts/SaveData/SavePosition.cs
+++ Assets/Scripts/SaveData/SavePosition.cs
@@ -10,10 +10,12 @@ public class SavePosition : MonoBehaviour
     public GameObject objetoPlayerTwo;
     public static int cargarPosicionInicial = 0;
     private NavMeshAgent positionObject;
+    public Camera camaraPosition;
 
     void Start()
     {
         this.positionObject = this.objetoPlayerTwo.GetComponent<NavMeshAgent>();
+
         CargarPosition();
     }
 
@@ -28,9 +30,10 @@ public class SavePosition : MonoBehaviour
 
             this.positionObject.destination = this.objetoPlayerTwo.transform.position;
 
-            /*objetoPlayerTwo.transform.position = new Vector3(PlayerPrefs.GetFloat("x2"),
-                PlayerPrefs.GetFloat("y2"), PlayerPrefs.GetFloat("z2"));
-            */
+            this.camaraPosition.transform.position = new Vector3(PlayerPrefs.GetFloat("xCam"),
+            PlayerPrefs.GetFloat("yCam"), PlayerPrefs.GetFloat("zCam"));
+
+
 
         }
 
@@ -48,6 +51,15 @@ public class SavePosition : MonoBehaviour
         PlayerPrefs.SetFloat("x2", objetoPlayerTwo.transform.position.x);
         PlayerPrefs.SetFloat("y2", objetoPlayerTwo.transform.position.y);
         PlayerPrefs.SetFloat("z2", objetoPlayerTwo.transform.position.z);
+
+        PlayerPrefs.SetFloat("xCam", camaraPosition.transform.position.x);
+        PlayerPrefs.SetFloat("yCam", camaraPosition.transform.position.y);
+        PlayerPrefs.SetFloat("zCam", camaraPosition.transform.position.z);
+
+        PlayerPrefs.SetFloat("rxCam", camaraPosition.transform.rotation.x);
+        PlayerPrefs.SetFloat("ryCam", camaraPosition.transform.rotation.y);
+        PlayerPrefs.SetFloat("rzCam", camaraPosition.transform.rotation.z);
+
     }
 
 }
